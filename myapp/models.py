343a59927077a3dda581ad2c3a171a7956c4056c
RAVI2530 <ravi.patil911@gmail.com>
2020-09-07 10:13:56 +0530

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+# Create your models here.
+
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=30,unique=True,blank=False)
+
+    def __str__(self):
+        return self.topic_name
+class Webpage(models.Model):
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100,unique=True,blank=False)
+    url=models.URLField(max_length=100,unique=True,blank=False)
+
+    def __str__(self):
+        return self.name
+class AccessDetails(models.Model):
+    webpage=models.ForeignKey(Webpage,on_delete=models.CASCADE)
+    datetime=models.DateTimeField()
+
+    def __str__(self):
+        return str(self.datetime).split(" ")[0]
+
